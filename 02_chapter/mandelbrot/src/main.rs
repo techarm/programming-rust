64eb965360f13bd54274f0b055824a5998e4f5e1
techarm <techarmdev@gmail.com>
2023-08-11 22:50:11 +0900

    Chapter 2.6 並列化

--- 02_chapter/mandelbrot/src/main.rs
+++ 02_chapter/mandelbrot/src/main.rs
@@ -23,7 +23,27 @@ fn main() {
     let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+
+    let thread = 8;
+    let rows_per_band = bounds.1 / thread + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        })
+        .unwrap();
+    }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file")
 }
@@ -39,8 +59,8 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     None
 }
 
-fn parse_pair<T: FromStr>(s: &str, spearator: char) -> Option<(T, T)> {
-    match s.find(spearator) {
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
         None => None,
         Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
             (Ok(l), Ok(r)) => Some((l, r)),
@@ -69,7 +89,7 @@ fn pixel_to_point(
     );
     Complex {
         re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im + pixel.1 as f64 * height / bounds.1 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
     }
 }
 
@@ -87,7 +107,7 @@ fn render(
             pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - count as u8,
-            }
+            };
         }
     }
 }
@@ -98,8 +118,15 @@ fn write_image(
     bounds: (usize, usize),
 ) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
+
     let encoder = PNGEncoder::new(output);
-    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
+
     Ok(())
 }
 
